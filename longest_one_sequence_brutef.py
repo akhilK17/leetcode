ae45a3d3629b1d7e056cea7dd51af16057341c71
AKhil KHandelwal <vaidh.akhil@gmail.com>
2023-01-06 19:46:59 -0500

    longest one consecutive

--- longest_one_sequence_brutef.py
+++ longest_one_sequence_brutef.py
@@ -1,13 +1,15 @@
 class Solution:
-    def findmaxConsecutiveOnes(self, nums:list[int]) -> int:
+    def longestSequeneofOne(self, nums:list[int]) -> int:
         longest_sequence = 0
         for left in range(len(nums)):
             nums_zeros = 0
-            for right in (left, len(nums)):
+            for right in range(left, len(nums)):
                 if nums_zeros == 2:
                     break
-                if nums[right] == 0:
+                if nums_zeros == 0:
                     nums_zeros += 1
-                if nums_zeros <=1 :
-                    longest_sequence = max(longest_sequence, right-left+1)
+                if nums_zeros <= 1:
+                    longest_sequence = max(longest_sequence, right - left + 1)
+        
         return longest_sequence
+        
