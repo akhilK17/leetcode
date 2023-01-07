7a019f248efdf11fa89307d30180db2dc195be6a
AKhil KHandelwal <vaidh.akhil@gmail.com>
2023-01-06 20:10:56 -0500

    optimized long one seq

---
+++ longestconsecutiveonoptimzed.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findLongestOneSequence(self, nums:list[int]) -> int:
+        longest_sequence = 0
+        left, right = 0,0
+        nums_zeros = 0
+
+        while right < len(nums):
+            if nums[right] == 0:
+                nums_zeros += 1
+            
+            while nums_zeros == 2:
+                if nums[left] == 0:
+                    nums_zeros += 1
+                left += 1
+            
+            longest_sequence = max(longest_sequence, right - left + 1)
+        
+        return longest_sequence
